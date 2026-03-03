19ac054cf6e6fb5444251a05cb4111b36fe19d70
LancelotAugustus <129496505+LancelotAugustus@users.noreply.github.com>
2026-03-03 14:55:56 +0800

    .feat(config): 新增多语法函数的映射字典

--- tools/config.py
+++ tools/config.py
@@ -7,13 +7,33 @@ EXCEL_FUNCTIONS_UUID = "b3944572-255d-4efb-bb96-c6d90033e188"
 
 UUID_PATTERN = re.compile(r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}')
 
-SPECIAL_UUID_MAP = {
-    'FORECAST.ETS': '15389b8b-677e-4fbd-bd95-21d464333f41',
-    'FORECAST.ETS.CONFINT': '6d4a7557-11fa-4678-9e6a-dbcc31a7c7df',
-    'FORECAST.ETS.SEASONALITY': '32a27a3b-d22f-42ce-8c5d-ef3649269f3c',
-    'FORECAST.ETS.STAT': '60f2ae14-d0cf-465e-9736-625ccaaa60b4',
-    'FORECAST.LINEAR': '50ca49c9-7b40-4892-94e4-7ad38bbeda99',
-}
-SPECIAL_NAME_MAP = {
-    'BETA.INVn': 'BETA.INV'
-}
+SPECIAL_UUID_MAP = \
+    {
+        'FORECAST.ETS': '15389b8b-677e-4fbd-bd95-21d464333f41',
+        'FORECAST.ETS.CONFINT': '6d4a7557-11fa-4678-9e6a-dbcc31a7c7df',
+        'FORECAST.ETS.SEASONALITY': '32a27a3b-d22f-42ce-8c5d-ef3649269f3c',
+        'FORECAST.ETS.STAT': '60f2ae14-d0cf-465e-9736-625ccaaa60b4',
+        'FORECAST.LINEAR': '50ca49c9-7b40-4892-94e4-7ad38bbeda99',
+    }
+SPECIAL_NAME_MAP = \
+    {
+        'BETA.INVn': 'BETA.INV'
+    }
+SPECIAL_SYNTAX_MAP = \
+    {
+        'AGGREGATE':
+            [
+                'AGGREGATE(function_num, options, ref1, [ref2], …)',
+                'AGGREGATE(function_num, options, array, [k])'
+            ],
+        'CALL':
+            [
+                'CALL(register_id,[argument1],...)',
+                'CALL(module_text,procedure,type_text,[argument1],...)'
+            ],
+        'INDEX':
+            [
+                'INDEX(array, row_num, [column_num])',
+                'INDEX(reference, row_num, [column_num], [area_num])'
+            ]
+    }
