9116dfdd8dd97198eeea107d0e9f31cf710ee9ef
LancelotAugustus <129496505+LancelotAugustus@users.noreply.github.com>
2026-03-09 20:10:10 +0800

    .chore(excode): 重构探索性代码

--- tools/777.py
+++ tools/777.py
@@ -11,12 +11,8 @@ def make_url(uuid):
     return url
 
 
-def test():
-    data = load_json("excel_functions.json")
-    for times, item in enumerate(data, 1):
-        name = item.get("func_name")
-        uuid = item.get("func_uuid")
-        soup = build_soup(uuid, make_url)
+def _get_syntax_string(func_name, func_uuid):
+        soup = build_soup(func_uuid, make_url)
         text = get_text(soup)
 
         # 找到第一个‘ Syntax ’移除掉所有在此之前的内容，并移除两端空格
@@ -28,21 +24,21 @@ def test():
             text = text[1:].strip()
 
         # 将所有name文本转为大写
-        pattern = re.compile(re.escape(name), re.IGNORECASE)
+        pattern = re.compile(re.escape(func_name), re.IGNORECASE)
         text = pattern.sub(lambda m: m.group(0).upper(), text)
 
         # 找到所有name文本，移除两端空格
-        pattern = re.compile(r'\s*' + re.escape(name) + r'\s*', re.IGNORECASE)
-        text = pattern.sub(name, text)
+        pattern = re.compile(r'\s*' + re.escape(func_name) + r'\s*', re.IGNORECASE)
+        text = pattern.sub(func_name, text)
 
         # 找到第一个前面不为the的{name}(，移除所有在此之前的内容
-        pattern = re.compile(r'(?<!the)' + re.escape(name) + r'\(', re.IGNORECASE)
+        pattern = re.compile(r'(?<!the)' + re.escape(func_name) + r'\(', re.IGNORECASE)
         match = pattern.search(text)
         if match:
             text = text[match.start():]
         else:
             paren_index = text.find('(')
-            text = name + text[paren_index:]
+            text = func_name + text[paren_index:]
 
         # 移除所有右括号之后的文本
         right_paren_index = text.find(')')
@@ -52,24 +48,38 @@ def test():
         if 'lambda(' in text:
             text += ')'
 
-        # 替换相关
-        text = text.replace('((', '(')
-        text = text.replace('-', '_')
-        text = text.replace(' ', '')
-        text = text.replace('\xa0', '')
-        text = text.replace('…', '...')
-        text = text.replace(',...', '...')
-        text = text.replace('...', ',...')
-        text = text.replace('...,', '...')
+        return text
 
-        # re.sub
-        text = re.sub(r'(?<!\[),(?=\[)', '', text)
-        text = re.sub(r'(?<![(,])\[', ',[', text)
-        text = re.sub(r'](?![),])', '],', text)
-        text = re.sub(r'\(([^()]*)\)', lambda m: m.group().lower(), text)
 
-        print(f"{times}. {name}")
-        print(text)
+def _clean_syntax_string(syntax_string):
+    # 替换相关
+    syntax_string = syntax_string.replace('((', '(')
+    syntax_string = syntax_string.replace('-', '_')
+    syntax_string = syntax_string.replace(' ', '')
+    syntax_string = syntax_string.replace('…', '...')
+    syntax_string = syntax_string.replace(',...', '...')
+    syntax_string = syntax_string.replace('...', ',...')
+    syntax_string = syntax_string.replace('...,', '...')
+
+    # re.sub
+    syntax_string = re.sub(r'(?<!\[),(?=\[)', '', syntax_string)
+    syntax_string = re.sub(r'(?<![(,])\[', ',[', syntax_string)
+    syntax_string = re.sub(r'](?![),])', '],', syntax_string)
+    syntax_string = re.sub(r'\(([^()]*)\)', lambda m: m.group().lower(), syntax_string)
+    return syntax_string
+
+
+def test():
+    data = load_json("excel_functions.json")
+    for times, item in enumerate(data, 1):
+        func_name = item.get("func_name")
+        func_uuid = item.get("func_uuid")
+
+        syntax_string = _get_syntax_string(func_name, func_uuid)
+        syntax_string = _clean_syntax_string(syntax_string)
+
+        print(f"{times}. {func_name}")
+        print(syntax_string)
         print("-" * 60)
 
 
